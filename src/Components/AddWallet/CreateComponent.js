b334ec7e12bc83c668cd742d6d8447969d79456c
John Savadkuhi <physitime@gmail.com>
2019-03-28 11:51:14 +0430

    update

--- src/Components/AddWallet/CreateComponent.js
+++ src/Components/AddWallet/CreateComponent.js
@@ -3,7 +3,11 @@ import GridWrap from '../../Utils/Grid'
 import {withStyles} from "@material-ui/core";
 import {tu} from "../../Utils/i18n";
 import Divider from "@material-ui/core/Divider";
-
+import CustomizedInputBase from '../Input'
+import IconLabelButtons from '../Button'
+import CloseIcon from '@material-ui/icons/Close'
+import CheckIcon from '@material-ui/icons/Check'
+import Button from "@material-ui/core/Button";
 const styles = theme => ({
     header: {
         textAlign: 'center',
@@ -20,7 +24,10 @@ const styles = theme => ({
     textMuted: {
         color: '#6c757d !important'
 
-    }
+    },
+    iconSmall: {
+        fontSize: 15,
+    },
 });
 
 const CreateComponent = props => {
@@ -38,12 +45,30 @@ const CreateComponent = props => {
                     <span style={{color: '#d12114'}}>tronwallet.network</span>
                 </small>
             </div>
-<br/>
-            <Divider variant="middle" light={true} />
+            <br/>
+            <Divider variant="middle" light={true}/>
+
+
+            <p className={classes.textMuted}>* Enter password to encrypt the private key</p>
+
+            <CustomizedInputBase placeHolder={'Password'}/>
+
+            <br/>
+            <CustomizedInputBase placeHolder={'Repeate Password'}/>
+            <br/>
+
             <div>
+                <span style={{fontSize:'12px'}} className={classes.textMuted}><CheckIcon className={classes.iconSmall}/> Enter at least 8 characters</span>
+                <span>  </span>
 
-               <p className={classes.textMuted}> Enter password to encrypt the private key</p>
+            </div>
+            <div className={classes.center}>
 
+                <Button variant="contained" size="small" >
+                    Next
+                    {/*<SaveIcon className={classNames(classes.rightIcon, classes.iconSmall)} />*/}
+
+                </Button>
 
             </div>
 
