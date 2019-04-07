e925ce9cc3d7cafca0992d2ef6c85cf95551e337
johnsavadkuhi <physitime@gmail.com>
2019-04-07 16:21:26 +0430

    add steeper

--- src/Components/AddWallet/CreateAccount.js
+++ src/Components/AddWallet/CreateAccount.js
@@ -2,8 +2,12 @@ import React, {lazy, Suspense} from 'react'
 import GridWrap from '../../Utils/Grid'
 import {useStateValue} from "../../State/StateProvider";
 import logo from "../../Images/mainLogo.png";
-import {makeStyles} from "@material-ui/core";
+import {makeStyles , useTheme} from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 //import AccountInfoForm from "./AccountInfoForm";
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import MobileStepper from "@material-ui/core/MobileStepper";
 
 const PasswordForm = lazy(() => import ("./PasswordForm"));
 const KeyStoreForm = lazy(() => import ("./KeyStoreForm"));
@@ -24,6 +28,9 @@ const useStyles = makeStyles(theme => ({
 const CreateAccount = props => {
 
     const classes = useStyles();
+    const theme = useTheme();
+    const maxSteps = 3;
+    const [activeStep, setActiveStep] = React.useState(0);
 
     const [{pass = {password: '', repeatPassword: ''}}] = useStateValue();
 
@@ -31,22 +38,85 @@ const CreateAccount = props => {
 
     const passwordIsValid = (!(pass.password === pass.repeatPassword && pass.password.length >= 8));
 
+    const handleDisabled = ()=>{
+
+        switch (activeStep) {
+
+            case 0 :
+
+                return !(activeStep === maxSteps - 1) && (passwordIsValid);
+            case 1 :
+
+                return !(activeStep === maxSteps - 1) && (!keyStore.downloaded) ;
+
+            default :
+                return (activeStep === maxSteps - 1) ;
+
+
+        }
+    };
+
+    const handleNext = ()=>{
+        setActiveStep(prevActiveStep => prevActiveStep + 1);
+
+
+    };
+    const handleBack = ()=>{
+        setActiveStep(prevActiveStep => prevActiveStep - 1);
+
+    } ;
     return (<>
 
         <GridWrap>
 
+            {console.log("direction : " , theme.direction )}
+            <div>
+
             <div className={classes.center}>
+
                 <img src={logo} alt={'tronLogo'} width={'150px'}/>
 
             </div>
 
-            {
-                keyStore.downloaded ?<Suspense feedback={<div>loading....</div>}><AccountInfoForm/></Suspense> :
-                passwordIsValid ? <Suspense feedback={<div>loading....</div>}> <PasswordForm/> </Suspense> :
-                    <Suspense feedback={<div>loading....</div>}> <KeyStoreForm/> </Suspense>
+            {activeStep===0 ? <Suspense feedback={<div>loading....</div>}> <PasswordForm/> </Suspense> :
+
+            activeStep === 1 ? <Suspense feedback={<div>loading....</div>}> <KeyStoreForm/> </Suspense> :
+                <Suspense feedback={<div>loading....</div>}><AccountInfoForm/></Suspense>
+
             }
+            {/*{*/}
+                {/*keyStore.downloaded ?<Suspense feedback={<div>loading....</div>}><AccountInfoForm/></Suspense> :*/}
+                {/*passwordIsValid ? <Suspense feedback={<div>loading....</div>}> <PasswordForm/> </Suspense> :*/}
+                    {/*<Suspense feedback={<div>loading....</div>}> <KeyStoreForm/> </Suspense>*/}
+            {/*}*/}
 
+            <MobileStepper
 
+                className={classes.textMuted}
+                steps={maxSteps}
+                position="static"
+                variant="text"
+                activeStep={activeStep}
+                nextButton={
+                    <Button size="small" onClick={handleNext} disabled={handleDisabled() }>
+
+                        Next
+                        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+
+                    </Button>
+                }
+                backButton={
+
+                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+
+                        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+
+                        Back
+
+                    </Button>
+                }
+            />
+            </div>
         </GridWrap>
 
     </>)
